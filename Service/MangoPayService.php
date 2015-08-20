56e4e1fc49816c0b02f273cd6f15d06122b13bb5
Olivier Hoareau <olivier@phppro.fr>
2015-08-20 10:00:08 +0200

    add MangoPay dependency

---
+++ Service/MangoPayService.php
@@ -0,0 +1,215 @@
+<?php
+
+/*
+ * This file is part of the VELOCITY package.
+ *
+ * (c) PHPPRO <opensource@phppro.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Velocity\Bundle\ApiBundle\Service;
+
+use MangoPay\Money;
+use MangoPay\PayIn;
+use MangoPay\Wallet;
+use MangoPay\Transfer;
+use MangoPay\MangoPayApi;
+use MangoPay\BankAccount;
+use MangoPay\UserNatural;
+use MangoPay\BankAccountDetailsIBAN;
+use MangoPay\BankAccountDetailsOTHER;
+use MangoPay\PayInPaymentDetailsCard;
+use MangoPay\PayInExecutionDetailsWeb;
+use MangoPay\Exception as MangoPayException;
+use Velocity\Bundle\ApiBundle\Traits\ServiceTrait;
+use Velocity\Bundle\ApiBundle\Traits\MangoPayApiAwareTrait;
+
+/**
+ * MangoPay Service.
+ *
+ * @author Olivier Hoareau <olivier@phppro.fr>
+ */
+class MangoPayService
+{
+    use ServiceTrait;
+    use MangoPayApiAwareTrait;
+    /**
+     * Constructs a new service
+     *
+     * @param MangoPayApi $mangoPayApi
+     */
+    public function __construct(MangoPayApi $mangoPayApi)
+    {
+        $this->setMangoPayApi($mangoPayApi);
+    }
+    /**
+     * Create a new MangoPay user.
+     *
+     * @param array $data
+     *
+     * @return array
+     *
+     * @throws MangoPayException
+     */
+    public function createUser(array $data)
+    {
+        $user = new UserNatural();
+
+        $user->FirstName          = $data['firstName'];
+        $user->LastName           = $data['lastName'];
+        $user->Email              = $data['email'];
+        $user->Birthday           = (new \DateTime($data['birthday']))->getTimestamp();
+        $user->CountryOfResidence = $data['countryOfResidence'];
+        $user->Nationality        = $data['nationality'];
+
+        return $this->prepareResult($this->getMangoPayApi()->Users->Create($user));
+    }
+    /**
+     * Create a new MangoPay wallet.
+     *
+     * @param array $data
+     *
+     * @return array
+     *
+     * @throws MangoPayException
+     */
+    public function createWallet(array $data)
+    {
+        $wallet = new Wallet();
+
+        $wallet->Currency    = $data['currency'];
+        $wallet->Description = $data['description'];
+        $wallet->Owners      = $data['owners'];
+
+        return $this->prepareResult($this->getMangoPayApi()->Wallets->Create($wallet));
+    }
+    /**
+     * Create a new MangoPay pay-in.
+     *
+     * @param array $data
+     *
+     * @return array
+     *
+     * @throws MangoPayException
+     */
+    public function createPayIn(array $data)
+    {
+        $payIn = new PayIn();
+
+        $payIn->AuthorId         = $data['authorId'];
+        $payIn->CreditedUserId   = $data['creditedUserId'];
+        $payIn->CreditedWalletId = $data['creditedWalletId'];
+        $payIn->DebitedFunds     = new Money();
+        $payIn->Fees             = new Money();
+        $payIn->PaymentDetails   = new PayInPaymentDetailsCard();
+        $payIn->ExecutionDetails = new PayInExecutionDetailsWeb();
+
+        $payIn->DebitedFunds->Currency = $data['currency'];
+        $payIn->DebitedFunds->Amount   = round($data['debitedFunds'] * 100, 0); // cents
+
+        $payIn->Fees->Currency = $data['currency'];
+        $payIn->Fees->Amount   = round($data['fees'] * 100, 0); // cents
+
+        $payIn->PaymentDetails->CardType = $data['cardType'];
+
+        $payIn->ExecutionDetails->Culture    = $data['culture'];
+        $payIn->ExecutionDetails->ReturnURL  = $data['returnUrl'];
+        $payIn->ExecutionDetails->SecureMode = $data['secureMode'];
+
+        return $this->prepareResult($this->getMangoPayApi()->PayIns->Create($payIn));
+    }
+    /**
+     * Create a new MangoPay transfer.
+     *
+     * @param array $data
+     *
+     * @return array
+     *
+     * @throws MangoPayException
+     */
+    public function createTransfer(array $data)
+    {
+        $transfer = new Transfer();
+
+        $transfer->AuthorId         = $data['authorId'];
+        $transfer->CreditedUserId   = $data['creditedUserId'];
+        $transfer->CreditedWalletId = $data['creditedWalletId'];
+        $transfer->DebitedWalletId  = $data['debitedWalletId'];
+        $transfer->DebitedFunds     = new Money();
+        $transfer->Fees             = new Money();
+
+        $transfer->DebitedFunds->Currency = $data['currency'];
+        $transfer->DebitedFunds->Amount   = round($data['debitedFunds'] * 100, 0); // cents
+
+        $transfer->Fees->Currency = $data['currency'];
+        $transfer->Fees->Amount   = round($data['fees'] * 100, 0); // cents
+
+        return $this->prepareResult($this->getMangoPayApi()->Transfers->Create($transfer));
+    }
+    /**
+     * Create a MangoPay bank account.
+     *
+     * @param array $data
+     *
+     * @return array
+     *
+     * @throws MangoPayException
+     */
+    public function createBankAccount(array $data)
+    {
+        $bankAccount = new BankAccount();
+
+        $bankAccount->OwnerName    = $data['ownerName'];
+        $bankAccount->OwnerAddress = $data['ownerAddress'];
+        $bankAccount->UserId       = $data['userId'];
+
+        switch($data['type']) {
+            case 'iban':
+                $bankAccount->Type = 'IBAN';
+
+                $details = new BankAccountDetailsIBAN();
+
+                $details->BIC  = $data['bic'];
+                $details->IBAN = $data['iban'];
+
+                $bankAccount->Details = $details;
+                break;
+            case 'other';
+                $bankAccount->Type = 'OTHER';
+
+                $details = new BankAccountDetailsOTHER();
+
+                $details->BIC           = $data['bic'];
+                $details->AccountNumber = $data['account'];
+                $details->Country       = $data['country'];
+
+                $bankAccount->Details = $details;
+                break;
+            default:
+                /** @noinspection PhpMethodParametersCountMismatchInspection */
+                $this->throwException(500, "Unsupported bank account type '%s'", $data['type']);
+        }
+
+        return $this->prepareResult(
+            $this->getMangoPayApi()->Users->CreateBankAccount($data['userId'], $bankAccount)
+        );
+    }
+    /**
+     * Convert the specified raw MangoPay result to array.
+     *
+     * @param mixed $raw
+     *
+     * @return array
+     */
+    protected function prepareResult($raw)
+    {
+        $raw = (array)$raw;
+
+        return array_combine(
+            array_map(function ($k) { return lcfirst($k);}, array_keys($raw)),
+            array_values($raw)
+        );
+    }
+}
---
+++ Service/ServiceFactoryService.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the VELOCITY package.
+ *
+ * (c) PHPPRO <opensource@phppro.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Velocity\Bundle\ApiBundle\Service;
+
+use MangoPay\MangoPayApi;
+
+/**
+ * Service Factory Service.
+ *
+ * @author Olivier Hoareau <olivier@phppro.fr>
+ */
+class ServiceFactoryService
+{
+    /**
+     * Create a new MangoPayApi instance.
+     *
+     * @param string $environment
+     * @param string $clientId
+     * @param string $clientPassword
+     * @param array  $options
+     *
+     * @return MangoPayApi
+     */
+    public function createMangoPayApi($environment, $clientId, $clientPassword, array $options = [])
+    {
+        $api = new MangoPayApi();
+
+        $api->Config->ClientId        = $clientId;
+        $api->Config->ClientPassword  = $clientPassword;
+        $api->Config->TemporaryFolder = sys_get_temp_dir();
+
+        switch($environment) {
+            case 'live':    $api->Config->BaseUrl = 'https://api.mangopay.com'; break;
+            case 'sandbox': $api->Config->BaseUrl = 'https://api.sandbox.mangopay.com'; break;
+            default:        $api->Config->BaseUrl = $environment; break;
+        }
+
+        foreach($options as $key => $value) {
+            $api->Config->$key = $value;
+        }
+
+        return $api;
+    }
+}
