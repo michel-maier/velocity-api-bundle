7786946b0bc3c6caafa8cd48f414ba6a8d565a21
Olivier Hoareau <olivier@phppro.fr>
2015-08-20 10:20:05 +0200

    add several annotation

---
+++ Annotation/Refresh/OnPropertyValueSetTo.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the VELOCITY package.
+ *
+ * (c) PHPPRO <opensource@phppro.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Velocity\Bundle\ApiBundle\Annotation\Refresh;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * OnPropertyValueSetTo Refresh annotation
+ *
+ * @author Olivier Hoareau <olivier@phppro.fr>
+ *
+ * @Annotation
+ * @Target("PROPERTY")
+ */
+final class Refresh extends Annotation
+{
+    public $unset = false;
+}
